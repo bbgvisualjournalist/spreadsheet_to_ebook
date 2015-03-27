07fcb0a9a75cc2eaac60cc9f0a9768d547bf868e
Brian Williamson <brianwilliamson@Brians-MacBook-Pro.local>
2015-03-27 12:16:05 -0400

    MINOR: removed users route

--- app.js
+++ app.js
@@ -95,7 +95,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
